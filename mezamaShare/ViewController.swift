b3634d1fc4036607b3ae9b6d8f3acb3fc266017b
AKIO <maizima44ww@gmail.com>
2016-10-13 09:48:15 +0900

    Init xcode project.

---
+++ mezamaShare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mezamaShare
+//
+//  Created by 板谷晃良 on 2016/10/13.
+//  Copyright © 2016年 AkkeyLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
