1b434ee78bb996da4e9436d58e4728d0cd8b0541
ellieny <fuakotok@gmail.com>
2016-10-29 19:30:33 +0900

    add Id class

--- mezamaShare/Classes/ViewController/Id.swift
+++ mezamaShare/Classes/ViewController/Id.swift
@@ -6,10 +6,34 @@
 //  Copyright © 2016年 AkkeyLab. All rights reserved.
 //
 
+/*import RealmSwift
+ 
+ 
+ let me = Id()
+ me.id = "akkey"
+ 
+ let realm = try! Realm()
+ //書き込み
+ try! realm.write {
+ realm.add(me)
+ }
+ //取得
+ for user in realm.objects(Id.self) {
+ NSLog("result\(user)")
+ }
+ 
+ realm.objects(Id.self)
+ //全件削除
+ try! realm.write({
+ realm.deleteAll()
+ })
+
+ */
+
 import UIKit
 import RealmSwift
 
 class Id: Object {
-    dynamic var ID = String()
+    dynamic var id: String = ""
 }
 
