a0298cebb6b56a4b15024e4f59faf82295fc6091
ellieny <fuakotok@gmail.com>
2016-10-29 17:13:36 +0900

    add UserDef class

---
+++ mezamaShare/Classes/ViewController/UserDef.swift
@@ -0,0 +1,13 @@
+//
+//  UserDef.swift
+//  mezamaShare
+//
+//  Created by Ellie NAGAISHI on 2016/10/29.
+//  Copyright © 2016年 AkkeyLab. All rights reserved.
+//
+
+import UIKit
+
+class UserDef: NSObject {
+
+}
