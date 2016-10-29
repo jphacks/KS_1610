5bf5a33c1c77f7ac04ba516382365bf3c3e8423c
Akkey <maizima44ww@gmail.com>
2016-10-29 16:50:28 +0900

    Make wake up screen XIB.

---
+++ mezamaShare/Classes/View/StampButtonView.swift
@@ -0,0 +1,37 @@
+//
+//  StampButtonView.swift
+//  mezamaShare
+//
+//  Created by Akio Itaya on 2016/10/29.
+//  Copyright © 2016年 AkkeyLab. All rights reserved.
+//
+
+import UIKit
+
+class StampButtonView: UIView {
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    
+    @IBAction func onTapButton1(_ sender: AnyObject) {
+    }
+    
+    @IBAction func onTapButton2(_ sender: AnyObject) {
+    }
+    
+    @IBAction func onTapButton3(_ sender: AnyObject) {
+    }
+    
+    @IBAction func onTapButton4(_ sender: AnyObject) {
+    }
+    
+    @IBAction func onTapButton5(_ sender: AnyObject) {
+    }
+    
+    @IBAction func onTapButton6(_ sender: AnyObject) {
+    }
+}
