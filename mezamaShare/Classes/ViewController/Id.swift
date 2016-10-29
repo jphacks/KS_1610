b2295e54e7f3989a3d164935a993b5820342dde3
ellieny <fuakotok@gmail.com>
2016-10-29 19:25:21 +0900

    Add Id class(realm)

---
+++ mezamaShare/Classes/ViewController/Id.swift
@@ -0,0 +1,15 @@
+//
+//  IdRealm.swift
+//  mezamaShare
+//
+//  Created by Ellie NAGAISHI on 2016/10/29.
+//  Copyright © 2016年 AkkeyLab. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class Id: Object {
+    dynamic var ID = String()
+}
+
