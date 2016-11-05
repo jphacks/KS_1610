1dc5a0b059fbfb101acf2c1cc0f7bb3cab9a3284
AKIO <maizima44ww@gmail.com>
2016-11-06 08:43:48 +0900

    FIx enter room paging.

--- mezamaShare/Classes/ViewController/MainViewController.swift
+++ mezamaShare/Classes/ViewController/MainViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2016年 AkkeyLab. All rights reserved.
 //
 import UIKit
+import RealmSwift
 
 class MainViewController: UIViewController {
 
@@ -107,7 +108,25 @@ class MainViewController: UIViewController {
                 self._listPeerIds.append(strValue)
             }
             if self._listPeerIds.count > 0 {
-                self.showPeerDialog()
+                let youID = Id()
+                let realm = try! Realm()
+                
+                try! realm.write({
+                    realm.deleteAll()
+                })
+                
+                for data in self._listPeerIds {
+                    youID.id = data
+                    
+                    try! realm.write {
+                        realm.add(youID)
+                    }
+                }
+                DispatchQueue.main.async {
+                    self.pagingView(selfNib: "", nibName: "EnterRoomView")
+                }
+            } else {
+                self.alertView(title: "未検出", message: "ペアリング相手が見つかりません", buttonName: "OK")
             }
         })
     }
@@ -142,26 +161,6 @@ class MainViewController: UIViewController {
         }
     }
     
-    func showPeerDialog() {
-        let vc: PeerListForDataViewController = PeerListForDataViewController()
-        vc.items = _listPeerIds as [AnyObject]?
-        vc.callback = self
-        
-        let nc: UINavigationController = UINavigationController.init(rootViewController: vc)
-        
-        DispatchQueue.main.async(execute: {
-            self.present(nc, animated: true, completion: nil)
-        })
-    }
-    
-    @IBAction func pushCallButton(_ sender: AnyObject) {
-        if _data == nil {
-            self.getPeerList()
-        } else {
-            self.performSelector(inBackground: #selector(MainViewController.close), with: nil)
-        }
-    }
-    
     func alertView(title: String, message: String, buttonName: String){
         let alert: UIAlertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let buttonAction = UIAlertAction(title: buttonName, style: .default) { action in
@@ -183,10 +182,10 @@ extension MainViewController {
     }
     
     func onHomeEnterRoom(sender: UIButton) {
-        if _bEstablished {
-            pagingView(selfNib: "", nibName: "EnterRoomView")
-        }else{
-            alertView(title: "接続未完了", message: "接続を完了させてください", buttonName: "OK")
+        if _data == nil {
+            self.getPeerList()
+        } else {
+            //
         }
     }
     
@@ -200,6 +199,19 @@ extension MainViewController {
     }
     
     func onEnterRoom(sender: UIButton) {
+        pagingView(selfNib: "", nibName: "WaitTimeView")
+        timer = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(self.update), userInfo: nil, repeats: false)
+    }
+    
+    func onEnterBuddyRoom(sender: UIButton) {
+        let realm = try! Realm()
+        var outString: String = ""
+        
+        for user in realm.objects(Id.self) {
+            outString = user.id
+        }
+        connect(outString)
+        
         pagingView(selfNib: "", nibName: "WaitTimeView")
         timer = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(self.update), userInfo: nil, repeats: false)
     }
