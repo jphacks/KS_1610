5c382cb70b1a10f7fed6102bd40ead16713c932e
AKIO <maizima44ww@gmail.com>
2016-11-19 03:29:56 +0900

    tmp

--- mezamaShare/Classes/View/Source/WaitTimeView.swift
+++ mezamaShare/Classes/View/Source/WaitTimeView.swift
@@ -11,7 +11,9 @@ import UIKit
 class WaitTimeView: UIView {
     @IBOutlet weak var roomNameLabel: UILabel!
     private let userDefault = UserDefaults.standard
-
+    var timer: Timer!
+    var stringDate: String = "00:00"
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
@@ -21,10 +23,23 @@ class WaitTimeView: UIView {
             roomNameLabel.text = userDefault.object(forKey: "myID") as? String
         }
         
-        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: Selector(("checkTime:")), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.checkTime), userInfo: nil, repeats: true)
+        timer.fire()
     }
     
     func checkTime(sender: Timer) {
-        print("on timer")
+        let now = NSDate()
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        
+        print(formatter.string(from: now as Date))
+        
+        
+        if userDefault.object(forKey: "setTime") != nil {
+            stringDate = userDefault.object(forKey: "setTime") as! String
+        }
+        print(stringDate)
+        print(formatter.date(from: stringDate))
     }
 }
